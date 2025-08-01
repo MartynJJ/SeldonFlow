e1cd02c7b16158ffeeb0f0599460982976a836f8
martyn <martyn@jepson.dev>
2025-08-01 18:57:30 +0000

    format update

--- src/seldonflow/execution/execution_manager.py
+++ src/seldonflow/execution/execution_manager.py
@@ -1,8 +1,8 @@
 from seldonflow.api_client.api_client import iApiClient
 
+
 class ExecutionManager:
     _api_client: iApiClient
+
     def __init__(self, api_client: iApiClient):
         self._api_client = api_client
-        
-    
