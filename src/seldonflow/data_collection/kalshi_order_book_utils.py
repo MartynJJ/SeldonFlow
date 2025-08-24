52cd83c8d8cd2f9fc076e806ed678775e34db0dd
martyn <martyn@jepson.dev>
2025-08-24 19:06:24 +0000

    improve ordrbook collection

--- src/seldonflow/data_collection/kalshi_order_book_utils.py
+++ src/seldonflow/data_collection/kalshi_order_book_utils.py
@@ -6,6 +6,13 @@ import numpy as np
 from typing import Dict, List, Union, Optional
 
 _COLUMN_RANGE = np.arange(1, 100, dtype=np.int32)
+_EXTRA_COLS = [
+    "TOP_YES_BID_PRICE",
+    "TOP_YES_BID_SIZE",
+    "TOP_YES_ASK_PRICE",
+    "TOP_YES_ASK_SIZE",
+]
+_FULL_COLS = list(_COLUMN_RANGE) + _EXTRA_COLS
 
 
 def create_orderbook_dataframe(
@@ -18,22 +25,34 @@ def create_orderbook_dataframe(
     """
     orderbook = orderbook_data["orderbook"]
     data = np.zeros(99, dtype=np.int32)
+    extra_arr = np.zeros(4, dtype=np.int32)
 
-    def process_data(data_array, sign: int):
+    def process_data(data_array, sign: int, extra_idx: List[int]):
         if not data_array:
+            if sign == 1:
+                np.add.at(extra_arr, extra_idx, [0, 0])
+            else:
+                np.add.at(extra_arr, extra_idx, [100, 0])
             return
-        arr = np.asarray(data_array, dtype=np.int32)
-        indices = arr[:, 0] - 1
-        values = arr[:, 1] * sign
 
+        arr = np.asarray(data_array, dtype=np.int32)
+        if sign == 1:
+            indices = arr[:, 0] - 1
+        else:
+            indices = 99 - arr[:, 0]
+        values = arr[:, 1] * sign
+        extra_arr_sub = np.array([indices[-1] + 1, values[-1]], dtype=np.int32)
         if np.any((indices < 0) | (indices >= 99)):
             raise ValueError("Orderbook indices must be between 1 and 99")
-
+        np.add.at(extra_arr, extra_idx, extra_arr_sub)
         np.add.at(data, indices, values)
 
-    process_data(orderbook.get("yes"), 1)
-    process_data(orderbook.get("no"), -1)
-
-    df = pd.DataFrame(data.reshape(1, -1), columns=_COLUMN_RANGE, index=[timestamp])
+    process_data(orderbook.get("yes"), 1, [0, 1])
+    process_data(orderbook.get("no"), -1, [2, 3])
+    df = pd.DataFrame(
+        np.append(data, extra_arr).reshape(1, -1),
+        columns=_FULL_COLS,
+        index=[timestamp],
+    )
     df.index.name = "timestamp"
     return df
