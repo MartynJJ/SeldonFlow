01bf98d7aee0246951aa2da8997f42cc6c9bb71e
martyn <martyn@jepson.dev>
2025-08-30 02:39:30 +0000

    updating run script

--- src/seldonflow/scripts/run.py
+++ src/seldonflow/scripts/run.py
@@ -1,14 +1,26 @@
 from seldonflow.platform.platform import LivePlatform
 from seldonflow.util.env import Environment
 
+import time
+
 # ENV = Environment.TESTING
 ENV = Environment.PRODUCTION
 
 
-def main():
-    print("SeldonFlow Platform starting...")
+def enable_platform():
     live_platform = LivePlatform(ENV)
     live_platform.enable()
+
+
+def main():
+    print("SeldonFlow Platform starting...")
+    while True:
+        try:
+            enable_platform()
+        except Exception as e:
+            print(f"Error in platform, pausing for 1 min and restarting {e}")
+            time.sleep(60)
+            continue
 
 
 if __name__ == "__main__":
